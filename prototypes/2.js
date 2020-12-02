792a9010ebff53035ff937a31ca891ea3bc12b63
Santtu <santtu.hietamaki@gmail.com>
2020-12-02 22:32:12 +0200

    Create 2.js

---
+++ prototypes/2.js
@@ -0,0 +1,14 @@
+// array version
+
+var input = document.body.getElementsByTagName("pre")[0].innerHTML
+	.trim().split("\n")
+	.map(line => line.split(/[- ]|: /))
+
+// 2-1 (buggy)
+input.map(line => line.concat([].concat(line[3].match(new RegExp(line[2], "g"))).length))
+	.filter(line => Number(line[0]) <= line[4] && Number(line[1]) >= line[4])
+	.length-1
+
+// 2-2
+input.filter(line =>
+  	(line[2] === line[3][line[0]-1]) !== (line[2] === line[3][line[1]-1]))
