93350a634de45f9f139ae35570824f387594a117
Santtu <santtu.hietamaki@gmail.com>
2020-12-02 22:32:35 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,14 +9,3 @@ numbers.flatMap(n1 => numbers.map(n2 => [n1 + n2, n1 * n2]))
 numbers.flatMap(n1 => numbers.map(n2 => [n1 + n2, n1 * n2]))
 	.flatMap(n1 => numbers.map(n2 => [n1[0] + n2, n1[1] * n2]))
 	.find(sum => sum[0] == 2020)
-
-
-// prototype (1-1 & 1-2)
-for (const number of numbers) {for (const number2 of numbers) {if ((Number(number)+Number(number2))==2020) console.log(number * number2) }}
-for (const number of numbers) {for (const number2 of numbers) {for (const number3 of numbers) if ((Number(number)+Number(number2)+Number(number3))==2020) console.log(number * number2 * number3) }}
-
-// prototype 2 (1-1)
-numbers.reduce((acc, number1) =>
-	acc > 0 ? acc :
-		(numbers.find(number2 =>
-			number1 + number2 == 2020) * number1 || 0), 0)
