ee401d7f461dbc897d2fdc4e7fddfb58972c26bf
Santtu <santtu.hietamaki@gmail.com>
2020-12-06 23:43:54 +0200

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,7 +1,21 @@
 // 6-1
-// Make sets of all the answers
+// Make answers into sets and get total size
 document.body.innerText
-    .split('\n\n')
-    .map(x => x.replaceAll('\n', ''))
-    .map(x => Array.from(new Set(x.split(''))))
-    .reduce((acc, x) => acc += x.length, 0)
+	.split('\n\n')
+	.map(x => x.replaceAll('\n', ''))
+	.map(x => new Set(x.split('')))
+	.reduce((acc, x) => acc += x.size, 0)
+
+// 6-2
+// Use first form of the group to get intersection of the sets
+document.body.innerText.trim()
+	.split('\n\n')
+	.map(group => group
+		.split('\n')[0]
+		.split('')
+		.filter(answer => group
+			.split('\n')
+			.every(form => form.includes(answer))
+		)
+	)
+	.reduce((acc, x) => acc += x.length, 0)
